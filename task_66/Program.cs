28b0fe704118a5970081f45fa4e2639450754ed1
Evgeny <buriy-anarchist@inbox.ru>
2022-08-02 15:23:11 +0300

    Add task66

---
+++ task_66/Program.cs
@@ -0,0 +1,18 @@
+﻿int m, n;
+Console.Write("введите число m: ");
+m = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число n: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+void NumberSum (int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine(sum); 
+        return;
+    }
+    sum = sum + (m++);
+    NumberSum(m, n, sum);
+}    
+
+NumberSum(m, n, 0);
